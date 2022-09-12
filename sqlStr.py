8a95eb9fdb9c518db52e1221b0012fc8faf92b2f
lesi_huang <lesi@auexpress.com.au>
2022-09-12 16:14:51 +1000

    finish sqlStr

--- sqlStr.py
+++ sqlStr.py
@@ -1,10 +1,45 @@
-mode = input("insert or update?")
+mode = input("insert or update? ")
+objName = input("object name? ")
 
-lines=[]
+data=[]
 line = input().strip()
 while line != "":
-    lines.append(line)
+    arr = line.split("\t")
+    data.append(arr)
     line = input().strip()
 
-for line in lines:
-    print(line)
+sqlStr = ""
+if mode.lower() in ("u", "update"):
+    sqlStr = "update  set "
+    for row in data:
+        sqlStr += f"{row[0]}=@{row[0]}, "
+    sqlStr = sqlStr.rstrip(", ")
+    sqlStr += " where id=@id"
+elif mode.lower() in ("i", "insert"):
+    sqlStr = "insert into  ("
+    for row in data:
+        sqlStr += f"{row[0]}, "
+    sqlStr.rstrip(", ")
+    sqlStr += ") values ("
+    for row in data:
+        sqlStr += f"@{row[0]}, "
+    sqlStr.rstrip(", ")
+    sqlStr += ")"
+
+print(sqlStr)
+print()
+
+if objName != "":
+    objName+="."
+
+if mode.lower() in ("u", "update"):
+        print(f"new SqlParameter(\"@id\", {objName}id),")
+
+for row in data:
+    if "char" in row[1] or "text" in row[1]:
+        print(f"new SqlParameter(\"@{row[0]}\", string.IsNullOrWhitespace({objName}{row[0]}) ? (object)DBNull.Value : {objName}{row[0]})),")
+    else:
+        print(f"new SqlParameter(\"@{row[0]}\", {objName}{row[0]}),")
+
+
+
